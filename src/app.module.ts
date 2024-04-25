624a7f60473052ac7e5c33b1c46a982cfb62469a
Vitor Berto <vitorberto.dev@gmail.com>
2024-04-24 23:26:13 -0300

    feat: redis cache config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbModule } from './db/db.module';
-import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MoviesModule } from './movies/movies.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
+    CacheModule.register((configService: ConfigService) => ({
+      isGlobal: true,
+      store: redisStore,
+      host: configService.get<string>('REDIS_HOST'),
+      port: +configService.get<number>('REDIS_PORT'),
+    })),
     ConfigModule.forRoot({ isGlobal: true }),
     DbModule,
     UsersModule,
@@ -16,6 +26,12 @@ import { MoviesModule } from './movies/movies.module';
     MoviesModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
